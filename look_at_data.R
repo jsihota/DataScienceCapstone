eecff51746f8b4ab9f7602990dcf0e5270a99254
jsihota <jsihota@gettyimages.com>
2015-07-09 13:20:06 -0700

    create n grams

--- look_at_data.R
+++ look_at_data.R
@@ -1,3 +1,10 @@
+#install.packages("tm")
+#install.packages("RWeka")
+
+library(tm)
+library(RWeka)
+
+
 
 setwd("~/class/DataScienceCapstone")
 # download file from URL
@@ -11,8 +18,16 @@ if (!file.exists("final")) {
 
 # OTHER GOOD Source will be google search terms, whatsup text messages 
 #load files
-en_US_twitter = read.table("final/en_US/en_US.twitter.txt", sep='\t', skipNul = TRUE,quote = "") 
-str(en_US_twitter)
+en_US_twitter = read.table("final/en_US/en_US.twitter.txt", sep='\t', skipNul = TRUE,quote = "",nrows = 500)
+en_US_news = read.table("final/en_US/en_US.news.txt", sep='\t', skipNul = TRUE,quote = "",nrows = 500) 
+en_US_blogs = read.table("final/en_US/en_US.blogs.txt", sep='\t', skipNul = TRUE,quote = "",nrows = 500) 
+
+dataset<-rbind(en_US_twitter, en_US_news)
+en_US_text <-rbind(dataset, en_US_blogs)
+rm(dataset)
+
+
+
 
 TrimSpace <- function( x ) {
   gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
@@ -23,12 +38,20 @@ TrimNonAlphanumeric<- function( x ) {
   gsub("[^[:alnum:] ]", "", x)
 }
 
+TrigramTokenizer <- function(x) NGramTokenizer(x, 
+                                               Weka_control(min = 3, max = 3))
+
+
 CleanUpEnglish <- function(x, output) {
-  text <- TrimSpace(TrimNonAlphanumeric(x[1]))
+  text <- tolower(TrimSpace(TrimNonAlphanumeric(x[1])))
   print(text)
+  tdm <- TermDocumentMatrix(Corpus(VectorSource(text)), 
+                            control = list(tokenize = TrigramTokenizer))
+  inspect(tdm)
+  
 }
 
-apply(en_US_twitter, 1, CleanUpEnglish, output = 'outputfile')
+apply(en_US_text, 1, CleanUpEnglish, output = 'outputfile')
 
 
 
